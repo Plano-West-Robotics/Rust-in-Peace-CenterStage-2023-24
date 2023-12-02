38c646d707248a26130e3e131fbd74241b81b1e6
tiggerbob <tiggerbob@gmail.com>
2023-12-01 23:00:57 -0600

    Prepare new servo limits and remove right arm servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/OuttakeArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/OuttakeArm.java
@@ -8,10 +8,8 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 public class OuttakeArm {
     private final Telemetry telemetry;
     public final Servo leftArm;
-    public final Servo rightArm;
 
     final double leftPos;
-    final double rightPos;
 
     public enum Position {
         UP,
@@ -20,13 +18,10 @@ public class OuttakeArm {
     public OuttakeArm(HardwareMap hardwareMap, Telemetry telemetry) {
         this.telemetry = telemetry;
         leftArm = hardwareMap.get(Servo.class,"LAservo");
-        rightArm = hardwareMap.get(Servo.class, "RAservo");
 
-        leftArm.scaleRange(0, 0.5);
-        rightArm.setDirection(Servo.Direction.REVERSE);
+        leftArm.scaleRange(0.1, 0.5);
 
         leftPos = leftArm.getPosition();
-        rightPos = rightArm.getPosition();
     }
 
     public void goTo(double targetPosition) {
@@ -35,9 +30,9 @@ public class OuttakeArm {
 
     public void goTo(Position targetPosition) {
         if (targetPosition == Position.UP) {
-            goTo(1);
+            goTo(0.9);
         } else if (targetPosition == Position.DOWN) {
-            goTo(0);
+            goTo(0.1);
         }
     }
 }
